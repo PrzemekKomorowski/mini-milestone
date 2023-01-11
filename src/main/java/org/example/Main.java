094292f61410e8a4f0b4d954fa79113908b79513
PrzemekKomorowski <106514184+PrzemekKomorowski@users.noreply.github.com>
2023-01-11 10:44:08 +0100

    dodanie-drugiego-lotu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,7 +15,7 @@ public class Main {
         airline.addFlight(flight1);
         airline.addFlight(flight2);
 
-        // Tworzymy pracowników
+        // Tworzymy pracowników lotu 1
         Pilot captain1 = new Pilot("John", "Wick", LocalDate.of(1980, 1, 1), "Captain");
         captain1.giveCompass();
         Pilot coPilot1 = new Pilot("Michael", "Jackson", LocalDate.of(1985, 2, 2), "Co-pilot");
@@ -26,10 +26,36 @@ public class Main {
         airline.addEmployee(coPilot1);
         airline.addEmployee(stewardess1);
 
+
+        // Tworzymy pracowników lotu 2
+        Pilot captain2 = new Pilot("Michael", "Jackson", LocalDate.of(1985, 2, 2), "Captain");
+        captain2.giveCompass();
+        Pilot coPilot2 = new Pilot("Przemek", "Komorowski", LocalDate.of(1980, 1, 1), "Co-pilot");
+        coPilot2.giveCompass();
+        Set<String> stewardessLanguages2 = new HashSet<>(Arrays.asList("English", "German"));
+        Stewardess stewardess2 = new Stewardess("Beata", "Gościnna", LocalDate.of(1990, 3, 3), stewardessLanguages2);
+        airline.addEmployee(captain2);
+        airline.addEmployee(coPilot2);
+        airline.addEmployee(stewardess2);
+
+
+
+
         // Przypisujemy pracowników do lotów
         airline.assignEmployeeToFlight(captain1.getFullName(), flight1.getFlightNumber());
         airline.assignEmployeeToFlight(coPilot1.getFullName(), flight1.getFlightNumber());
         airline.assignEmployeeToFlight(stewardess1.getFullName(), flight1.getFlightNumber());
+
+
+        airline.assignEmployeeToFlight(captain2.getFullName(), flight2.getFlightNumber());
+        airline.assignEmployeeToFlight(coPilot2.getFullName(), flight2.getFlightNumber());
+        airline.assignEmployeeToFlight(stewardess2.getFullName(), flight2.getFlightNumber());
+
+
+
+
+
+
 
 
 
@@ -44,6 +70,14 @@ public class Main {
         } else {
             System.out.println("Flight is not ready for departure");
             for (String step : flight1.getCheckSteps()) {
+                System.out.println(step);
+            }
+        }
+        if (flight2.isReadyForDeparture()) {
+            System.out.println("Flight is ready for departure");
+        } else {
+            System.out.println("Flight is not ready for departure");
+            for (String step : flight2.getCheckSteps()) {
                 System.out.println(step);
             }
         }
